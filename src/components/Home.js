28dc214771ee7b2cb5a6ed853fa2654fc0d42de7
Debayan <debayanghosh408@gmail.com>
2023-11-12 01:10:50 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,12 @@
-
 import "./home.css";
-import React from 'react';
-
-
-
-
+import React from "react";
 
 export default function Home() {
   return (
     <>
-    
-    
-    
-    <div className="container">
-
-
-    
-   
- 
-    <div className="card1 mx-2"></div>
-        
-   
- 
-        
-       
-   
-    </div>
-    
-   
+      <div className="container">
+        <div className="card1 mx-2"></div>
+      </div>
     </>
-  )
+  );
 }
